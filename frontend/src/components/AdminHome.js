a7d103819c3b64a84e491ab423d749228d3d5a63
MingyuanRen <m28ren@uwaterloo.ca>
2023-07-09 16:29:24 -0400

    just for test page

--- frontend/src/components/AdminHome.js
+++ frontend/src/components/AdminHome.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useSelector } from 'react-redux';
 import './AdminHome.css';
 
 export const AdminHome = () => {
+  const user = useSelector(state => state.user);
   return (
     <Container className="custom-container text-center">
       <Row className="justify-content-md-center">
         <Col md="auto">
-          <h1 className="title-text">Admin Welcome to WaterlooLibrary!</h1>
-          <p className="lead-text">CS 348 Project</p>
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center mt-3">
-        <Col md="auto">
-          <Link className="btn btn-primary btn-lg custom-button" to="/register">Register</Link>
-          <Link className="btn btn-success btn-lg custom-button" to="/login">Login</Link>
+          <h1 className="title-text">Hi {user ? user.name : ''}! You are Admin Welcome to WaterlooLibrary!</h1>
         </Col>
       </Row>
     </Container>
