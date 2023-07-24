5726b24aefe445df28a18061cdaf29d1260c396c
Jalynn Meng <j53meng@uwaterloo.ca>
2023-07-24 16:13:25 -0400

    Update book details page layout

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {useLocation} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { UserIcon } from "./filter.jsx";
+import { motion } from "framer-motion";
 import './BookDetails.css';
+import bookIcon from './book.png'
 
 export const BookDetails = () => {
     const location = useLocation();
@@ -40,14 +44,28 @@ export const BookDetails = () => {
     };
 
     return (
-        <div className="book-details-container">
-            <div className="book-details-card">
-                <h2>{book.title}</h2>
-                <p>Author: {book.author}</p>
-                <p>ISBN: {book.isbn}</p>
-                <button onClick={handleBorrow}>Borrow</button>
-                <button onClick={handleReserve}>Reserve</button>
-            </div>
+        <motion.div>
+        <div>
+            <Link to="/user-home">
+                <UserIcon/>
+            </Link>
         </div>
-    );
+        <div className="book-details-container">
+      <div className="left-content">
+        <div className="book-icon">
+          <img src={bookIcon} alt="Book Icon" />
+        </div>
+        <div className="buttons">
+          <button onClick={handleBorrow}>Borrow</button>
+          <button onClick={handleReserve}>Reserve</button>
+        </div>
+      </div>
+      <div className="book-details-card">
+        <h2>{book.title}</h2>
+        <p>Author: {book.author}</p>
+        <p>ISBN: {book.isbn}</p>
+      </div>
+    </div>
+    </motion.div>
+  );
 };
